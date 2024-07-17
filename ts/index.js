8cac20abd80e02e73df92c8fc33ff2fc4c567bae
Yeongi Park <yeongi0111@naver.com>
2024-07-17 20:27:35 +0900

    tuple 타입 숙제 해결

--- ts/index.js
+++ ts/index.js
@@ -1,20 +1,28 @@
-function 함수(x) {
-    console.log(x.length);
-    return;
-}
-함수("hello");
-함수(["kim", "park"]);
-function parse(x) {
-    return JSON.parse(x);
-}
-var data = '{"name" : "dog", "age" : 1 }';
-console.log(parse(data));
 // 3번
-var Person = /** @class */ (function () {
-    function Person(a) {
-        this.name = a;
+function 함수() {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
     }
-    return Person;
-}());
-var a = new Person("어쩌구");
-a.name;
+    console.log(rest);
+}
+함수("fff", true, 343, 43243, 4324324, "fadsfasd");
+// 4번
+function 함수2() {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
+    }
+    var num = [];
+    var str = [];
+    rest.forEach(function (item) {
+        if (typeof item === "string") {
+            str.push(item);
+        }
+        else {
+            num.push(item);
+        }
+    });
+    return [str, num];
+}
+console.log(함수2("b", 5, 6, 8, "a"));
--- ts/index.ts
+++ ts/index.ts
@@ -1,35 +1,24 @@
-// 1번 해결
-type Length = {
-  length: number;
-};
-
-function 함수<T extends string | string[]>(x: T) {
-  console.log(x.length);
-  return;
-}
-
-함수<string>("hello");
-함수<string[]>(["kim", "park"]);
-
-// 2 번
-interface Animal {
-  name: string;
-  age: number;
-}
-
-function parse<T>(x: string): T {
-  return JSON.parse(x);
-}
-
-let data = '{"name" : "dog", "age" : 1 }';
-console.log(parse<Animal>(data));
-
 // 3번
-class Person<T> {
-  name: T;
-  constructor(a: T) {
-    this.name = a;
-  }
+function 함수(...rest: [string, boolean, ...(string | number)[]]) {
+  console.log(rest);
 }
-let a = new Person<string>("어쩌구");
-a.name;
+
+함수("fff", true, 343, 43243, 4324324, "fadsfasd");
+
+// 4번
+function 함수2(...rest: (string | number)[]): [string[], number[]] {
+  let num: number[] = [];
+  let str: string[] = [];
+
+  rest.forEach((item) => {
+    if (typeof item === "string") {
+      str.push(item);
+    } else {
+      num.push(item);
+    }
+  });
+
+  return [str, num];
+}
+
+console.log(함수2("b", 5, 6, 8, "a"));
